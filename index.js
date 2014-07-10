e1d2d245ef99d29913c5ba9c1ec62e6ac0d247f3
theporchrat <theporchrat@gmail.com>
2014-07-10 17:29:25 -0400

    more more trait stuff

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 var BaseStore = require('./lib/stores/BaseStore')
   , defineStore = require('./lib/stores/define-store')
-  
-module.exports = {
+  , namespace = {};
+
+module.exports.create = require('./lib/Application').create
+
+module.exports.Stores = {
 
 	defineStore: defineStore,
 
-	actions: require('./lib/Actions'),
-
 	StoreWatchMixin: require('./lib/stores/StoreWatchMixin'),
 
-	DataHelperStoreMixin: require('./lib/stores/DataHelperStoreMixin'),
+	DataHelperStoreMixin: require('./lib/stores/DataHelperStoreMixin')
+
+}
+
+module.exports.DataAccess = {
+
+	Model: require('./lib/dal/Model'),
+
+	Collection: require('./lib/dal/Collection')
+}
+
+module.exports.Actions = require('./lib/actions/actions')
+
+module.exports.DataAccess = {
 
 	Model: require('./lib/dal/Model'),
 
